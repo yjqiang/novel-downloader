21a37734abd2aa5200329b6350f531b78b7367cd
yjqiang <8900942+yjqiang@users.noreply.github.com>
2021-08-04 23:20:22 +0800

    加入新的网站

--- test_crawler.py
+++ test_crawler.py
@@ -15,7 +15,8 @@ async def main():
 
     url = 'https://www.xbookcn.com/book/chest/1.htm'
     url = 'http://m.haohengwx.com/16/16248/177341.html'
-    # url = 'http://www.hejixs.com/113/113311/7150266_2.html'
+    url = 'http://www.hejixs.com/113/113311/7150270.html'
+    url = 'https://www.kunnu.com/zichuan/46250.htm'
     rule: WebsiteRule = rules[urlparse(url).netloc]
 
     session = dict_str_class.web_sessions[rule.all_rules_of_1_website['body_page']['loader']]()
@@ -55,9 +56,9 @@ async def main():
 
     url = 'https://www.xbookcn.com/book/chest/index.htm'
     url = 'http://m.haohengwx.com/16/16248/'
-    # url = 'http://www.hejixs.com/113/113311_1/'
+    url = 'http://www.hejixs.com/113/113311_1/'
+    url = 'https://www.kunnu.com/zichuan/'
     rule: WebsiteRule = rules[urlparse(url).netloc]
-    print(rule.website_setting_rule.headers)
 
     session = dict_str_class.web_sessions[rule.all_rules_of_1_website['index_page']['loader']]()
     await session.init()
